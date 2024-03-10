0ef75d3106f725c8134bb6264d75303aaf2c2fad
Yash Choudhary <4yashchoudhary@gmail.com>
2024-03-10 13:12:13 -0500

    ___ ka commit

--- exception.py
+++ exception.py
@@ -24,6 +24,6 @@ if __name__=='__main__':
     try:
         a=(1/0)
     except Exception as e:
-        logging.info("Divide by zero")
+        logging.info("Divide by zero time 2")
         raise CustomException(e,sys)
     
--- logger.py
+++ logger.py
@@ -1,6 +1,6 @@
 import logging
 import os
-from datetime import datetime
+
 
 LOG_FILE="Logging/Logging_info.log"
 
